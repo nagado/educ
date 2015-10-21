13a1770600cee34cf9e72c3077b8f47f31f2c40a
Elizaveta Kishchukova <elizabethnagado@gmail.com>
2015-10-21 10:59:35 -0400

    Window view

--- sfml/main.cpp
+++ sfml/main.cpp
@@ -1,24 +1,37 @@
 #include <SFML/Graphics.hpp>
 
+
+int window_x = 600;
+int window_y = 400;
+int line_width = 3;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600, 400), "Squash Game", sf::Style::Titlebar|sf::Style::Close);
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+  sf::RenderWindow window(sf::VideoMode(window_x, window_y), "Squash Game", sf::Style::Titlebar|sf::Style::Close);
+  sf::RectangleShape line_top(sf::Vector2f(window_x, line_width));
+  sf::RectangleShape line_left(sf::Vector2f(window_y, line_width));
+  sf::RectangleShape line_right(sf::Vector2f(window_y, line_width));
 
-    while (window.isOpen())
+  line_top.setPosition(0, line_width);
+  line_left.rotate(90.f);
+  line_left.setPosition(line_width * 2, 0);
+  line_right.rotate(90.f);
+  line_right.setPosition(window_x - line_width, 0);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+      if (event.type == sf::Event::Closed)
+        window.close();
     }
 
-    return 0;
+    window.clear();
+    window.draw(line_top);
+    window.draw(line_left);
+    window.draw(line_right);
+    window.display();
+  }
+
+  return 0;
 }
